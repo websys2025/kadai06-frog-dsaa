a2491af9df6879264524aff5f81cb3133e5d3bc1
j23072 <j23072et@edu.tuis.ac.jp>
2025-06-14 00:16:59 +0900

    update:kadai6-1の修正

--- kadai6-1.py
+++ kadai6-1.py
@@ -6,10 +6,12 @@ API_URL = "http://api.e-stat.go.jp/rest/3.0/app/json/getStatsData"
 params ={
     "appId": APP_ID,
     "statsDataId": "0001822415",#2018年漁業センサス/流通加工業/魚市場数
+    "cdArea": "1988000000",
     "lang": "J"
 }
 
 #エンドポイント
 response = requests.get(API_URL,params=params)
+
 data = response.json()
 print(data)
