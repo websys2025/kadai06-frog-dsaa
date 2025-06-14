dec59dcaab87faa72504f29d71b645127b3600ec
j23072 <j23072et@edu.tuis.ac.jp>
2025-06-15 00:04:31 +0900

    update]kadai6-1の修正

--- kadai6-1.py
+++ kadai6-1.py
@@ -1,3 +1,8 @@
+""""
+# オープンデータ名と概要:
+漁業センサス
+このAPIは、流通加工業における魚市場の数について提供している
+"""
 import requests
 
 APP_ID = "e740eb2247dff5f8c0b8c0dcad40b41c669abfc1"
@@ -6,7 +11,10 @@ API_URL = "http://api.e-stat.go.jp/rest/3.0/app/json/getStatsData"
 params ={
     "appId": APP_ID,
     "statsDataId": "0001822415",#2018年漁業センサス/流通加工業/魚市場数
-    "cdArea": "1988000000",
+    "statsCode": "00500210",
+    "searchWord": "累計",
+    "openYeaars": 20210322,
+    "limit": 2,
     "lang": "J"
 }
 
